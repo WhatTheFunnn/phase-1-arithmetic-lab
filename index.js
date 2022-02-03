585be203c1acb2edf9a600cb55319eee14ca5812
Dan Souza <Souza.music@outlook.com>
2022-02-02 22:45:53 -0800

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
-// Write your code here
+let num1 = 2;
+let num2 = 31;
+let num3 = 3;
+let num4 = 1;
+
+let multiply = (num1 * num2);
+let random = Number.isInteger(42, 70) +1;
+let mod = (num3 + num4);
+let max = Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ,15, 16, 17, 18, 19,20);
